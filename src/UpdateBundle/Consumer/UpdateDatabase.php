a76f755f81750652a5076efe374d4a18b9d13bf9
skafandri <kooli.ilyes@gmail.com>
2016-05-28 11:44:34 +0300

    format code

--- src/UpdateBundle/Consumer/UpdateDatabase.php
+++ src/UpdateBundle/Consumer/UpdateDatabase.php
@@ -1,40 +1,27 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace UpdateBundle\Consumer;
+
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 use Doctrine\Common\Persistence\ObjectManager;
 use UpdateBundle\Connection;
+
 /**
  * Description of UpdateDatabase
  *
  * @author ameni
  */
 class UpdateDatabase implements ConsumerInterface {
+
     private $documentManager;
+
     public function __construct(ObjectManager $om) {
-        
-
-
         $this->documentManager = $om;
     }
+
     public function execute(AMQPMessage $msg) {
         $message = $msg->body;
-     
-  
- 
-  
-    Connection::connection($this->documentManager)->packages->insert(json_decode($message, true),array("safe" => true,'upsert' => true));
- 
-    
-      
-     }
-  
-     
+        Connection::connection($this->documentManager)->packages->insert(json_decode($message, true), array("safe" => true, 'upsert' => true));
+    }
+
 }
--- src/UpdateBundle/Consumer/UpdatePackagesNames.php
+++ src/UpdateBundle/Consumer/UpdatePackagesNames.php
@@ -1,14 +1,6 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of UpdatePackagesNames
- *
  * @author ameni
  */
 
@@ -17,34 +9,22 @@ namespace UpdateBundle\Consumer;
 use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
 use PhpAmqpLib\Message\AMQPMessage;
 
-
-
 class UpdatePackagesNames implements ConsumerInterface {
 
-   
     private $urlDetailPackage;
     private $serviceStorePackagesDetails;
-    public function __construct($urlDetailPackage,$serviceStorePackagesDetails) {
-      
 
-
-        $this->urlDetailPackage=$urlDetailPackage;
-        $this->serviceStorePackagesDetails=$serviceStorePackagesDetails;
+    public function __construct($urlDetailPackage, $serviceStorePackagesDetails) {
+        $this->urlDetailPackage = $urlDetailPackage;
+        $this->serviceStorePackagesDetails = $serviceStorePackagesDetails;
     }
-     public function execute(AMQPMessage $msg) {
-        $message = $msg->body;
-     
-  $parameters= json_decode(file_get_contents($this->urlDetailPackage . $message. ".json"), true);
- 
-   $this->serviceStorePackagesDetails->process(json_encode($parameters,true));
-    
- 
-    
-      
-     }
-    
 
-    
-    
+    public function execute(AMQPMessage $msg) {
+        $message = $msg->body;
+
+        $parameters = json_decode(file_get_contents($this->urlDetailPackage . $message . ".json"), true);
+
+        $this->serviceStorePackagesDetails->process(json_encode($parameters, true));
+    }
 
 }
