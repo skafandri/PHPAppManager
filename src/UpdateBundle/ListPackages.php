078f01c989bf95637625e0f521a28c39f00187af
skafandri <kooli.ilyes@gmail.com>
2016-05-28 11:17:41 +0300

    Format code

--- src/UpdateBundle/ListPackages.php
+++ src/UpdateBundle/ListPackages.php
@@ -1,33 +1,28 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace UpdateBundle;
 
 /**
- * Description of ListPackages
- *
  * @author ameni
  */
 class ListPackages {
-    
+
     private $urlListPackages;
     private $urlDetailPackage;
     private $producer;
-     public function __construct($urlListPackages,$urlDetailPackage,$producer)
-    {
+
+    public function __construct($urlListPackages, $urlDetailPackage, $producer) {
         $this->urlListPackages = $urlListPackages;
-        $this->urlDetailPackage=$urlDetailPackage;
+        $this->urlDetailPackage = $urlDetailPackage;
         $this->producer = $producer;
     }
-   public function process(){
-       $packagesNames = json_decode(file_get_contents($this->urlListPackages), true)["packageNames"];
-       foreach ($packagesNames as $key=>$value){
-       $parameters= json_decode(file_get_contents($this->urlDetailPackage . $value. ".json"), true);
-       $this->producer->publish(json_encode($parameters,true));
-   }}
+
+    public function process() {
+        $packagesNames = json_decode(file_get_contents($this->urlListPackages), true)["packageNames"];
+        foreach ($packagesNames as $key => $value) {
+            $parameters = json_decode(file_get_contents($this->urlDetailPackage . $value . ".json"), true);
+            $this->producer->publish(json_encode($parameters, true));
+        }
+    }
+
 }
