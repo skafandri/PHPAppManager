c4358a8002a7ab0f3efc6483d7bb65a9bd218dda
ameni <amenikooli@gmail.com>
2016-05-31 16:23:24 +0100

    delete vendor

--- src/UpdateBundle/ListPackages.php
+++ src/UpdateBundle/ListPackages.php
@@ -7,27 +7,33 @@
  */
 
 namespace UpdateBundle;
-
+use UpdateBundle\Connection;
+use Doctrine\Common\Persistence\ObjectManager;
 /**
  * Description of ListPackages
  *
  * @author ameni
  */
 class ListPackages {
-    
+
     private $urlListPackages;
-    
+    private $documentManager;
     private $producer;
-     public function __construct($urlListPackages,$producer)
-    {
+
+    public function __construct($urlListPackages, $producer, ObjectManager $om) {
         $this->urlListPackages = $urlListPackages;
-  
+
+        $this->documentManager = $om;
         $this->producer = $producer;
     }
-   public function process(){
-       $packagesNames = json_decode(file_get_contents($this->urlListPackages), true)["packageNames"];
-       foreach ($packagesNames as $key=>$value){
-     
-       $this->producer->publish($value);
-   }}
+
+    public function process() {
+        $packagesNames = json_decode(file_get_contents($this->urlListPackages), true)["packageNames"];
+        Connection::connection($this->documentManager)->packages->drop();
+        foreach ($packagesNames as $key => $value) {
+            
+            $this->producer->publish($value);
+        }
+    }
+
 }
